05efb67b1016c7ea7689600cae505c4ea4c08b25
Jose <Alias_one11@outlook.com>
2019-10-28 20:23:01 +0100

    First commit in C++ refresher

---
+++ CPlusPlusUserInput/cmake-build-debug/CMakeFiles/CPlusPlusUserInput.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusPlusUserInput.dir/CPlusPlusDecomposition/DeCompositionMain.cpp.o"
+  "CMakeFiles/CPlusPlusUserInput.dir/CPlusPlusUserInput/main.cpp.o"
+  "CPlusPlusUserInput"
+  "CPlusPlusUserInput.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusPlusUserInput.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
