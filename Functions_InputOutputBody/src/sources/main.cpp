3b606abc2b51f5e8b546e8c024f957a590ac29a2
Jose <Alias_one11@outlook.com>
2019-10-30 13:21:23 +0100

    C++ Refresher

--- Functions_InputOutputBody/src/sources/main.cpp
+++ Functions_InputOutputBody/src/sources/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include <string>
+#include "MyFuncs.cpp"
 
 int main()
 {
+	auto* w = new Whatever;
+	w -> printAlias111();
 
+	auto num = 16;
+	w -> addOne(&num); // ! '&' @The address
+
+	auto num2 = 12;
+	w -> addTwo(num2);
 
 	std::cin.get();
 }
