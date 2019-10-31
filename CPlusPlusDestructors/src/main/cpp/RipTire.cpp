9dbde54e00684f9fe0eee742b55d46b432507356
Jose <Alias_one11@outlook.com>
2019-10-31 18:38:23 +0100

    C++ Refresher

---
+++ CPlusPlusDestructors/src/main/cpp/RipTire.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "RipTire.h"
+
+
+class RipTire {
+public:
+	RipTire() {
+		Log("Objected Constructed!!!")
+	}
+
+	~RipTire() {
+		Log("Objected Destructed!!!")
+	}
+};
