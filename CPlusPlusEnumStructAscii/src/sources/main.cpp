93ddf64bf628302e2895a66cb4a769193319a93c
Jose <Alias_one11@outlook.com>
2019-10-31 14:02:15 +0100

    C++ Refresher

--- CPlusPlusEnumStructAscii/src/sources/main.cpp
+++ CPlusPlusEnumStructAscii/src/sources/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include "../headers_H/MyLogic.h"
 using std::string;
 
+#define Log(x) std::cout << "-=>$: [ " << x << " ]" << std::endl;
+#define Log2(x) std::cout << x << std::endl;
 
+enum class ClassColor { red = 'a' };
+
+struct Cat {
+	int hairLength;
+	string hairColor;
+	int fatness = 5;
+};
 
 int main() {
-	std::cout << dog << '\n';// Will print out the index
-	std::cout << cat << '\n';// Will print out the index
-	std::cout << deer << '\n';// Will print out the index
-	std::cout << (dog == chicken) << '\n'; // True both are index 0 == 0
+	auto str1 = "_______________ ENUM EXAMPLE _______________\n"
+			    "{ Enums give scope to values }";
+	Log2(str1)
+	// Converting an ascii character to an int
+	auto red = static_cast<int>(ClassColor::red);
+	Log(red)// Console output '97'
 
-	auto h = chicken;
-	// * This works
-	auto e(eggs);// No need for the equals '='
+	// * STRUCT EXAMPLE
+	auto str2 = "_______________ STRUCT EXAMPLE _______________\n"
+			 "{ Structs: Aggregate data-type or way"
+			 " to package related vars together }";
 
+	Log2(str2)
 
 
 	std::cin.get();
