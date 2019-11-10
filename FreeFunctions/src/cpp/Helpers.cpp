820dcd34494b8bf57e9c6c05947203a1ecda0a26
Jose <Alias_one11@outlook.com>
2019-11-11 00:34:06 +0100

    C++ Learning

--- FreeFunctions/src/cpp/Helpers.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Jose Martinez on 11/7/19.
-//
-
-#include "Helpers.hpp"
--- FreeFunctions/src/headers/Helpers.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Jose Martinez on 11/7/19.
-//
-
-#ifndef FREEFUNCTIONS_HELPERS_HPP
-#define FREEFUNCTIONS_HELPERS_HPP
-
-#endif //FREEFUNCTIONS_HELPERS_HPP
